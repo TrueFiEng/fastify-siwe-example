48b56d61a13023e37102eb17b65b148e23b3262c
mateusz <mistrzumati@spoko.pl>
2022-04-29 12:45:00 +0200

    Update address

--- src/app.ts
+++ src/app.ts
@@ -55,7 +55,7 @@ fastify.get(
 
 const start = async () => {
   try {
-    await fastify.listen(5000)
+    await fastify.listen(5000, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
