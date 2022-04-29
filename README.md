8e3d54a2cd6af1fdf97d793dcdffbc5d4634597d
mateusz <mistrzumati@spoko.pl>
2022-04-29 11:53:28 +0200

    Add base backend

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fastify-siwe-example
+
+To run backend just type:
+
+```sh
+pnpm i && pnpm build && pnpm start
+```
---
+++ src/app.ts
@@ -0,0 +1,53 @@
+import createFastify, { FastifyInstance, FastifyRequest } from 'fastify'
+import { signInWithEthereum } from 'fastify-siwe'
+import cors from 'fastify-cors'
+
+const fastify = createFastify({ logger: true })
+
+void fastify.register(cors)
+void fastify.register(signInWithEthereum())
+
+fastify.post(
+  '/siwe/init',
+  {},
+  async function handler(
+    this: FastifyInstance,
+    req: FastifyRequest,
+    reply,
+  ) {
+    void reply.send({
+      nonce: await req.siwe.generateNonce(),
+    })
+  },
+)
+
+fastify.get(
+  '/siwe/me',
+  {},
+  async function handler(
+    this: FastifyInstance,
+    req: FastifyRequest,
+    reply,
+  ) {
+    if (!req.siwe.session) {
+      void reply.status(401).send()
+      return
+    }
+
+    void reply.code(200).send({
+      loggedIn: true,
+      message: req.siwe.session,
+    })
+  },
+)
+
+const start = async () => {
+  try {
+    await fastify.listen(3001)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+void start()
